cc184c457202e8de86440870eaf6e501dcd3e3e8
Adrian Ash <adrianash2705@gmail.com>
2019-11-16 16:02:48 +1000

    user complete with validation, just job type left to do

--- src/components/Data/CreateNewForm.js
+++ src/components/Data/CreateNewForm.js
@@ -1,16 +1,115 @@
 import React, { Component } from "react";
 import { connect } from 'react-redux'
 import { UsersActionsAdd } from "../../actions/UsersActions";
+import M from 'materialize-css'
 
 class CreateNewForm extends Component {
+    validation = passed_case => e => {
+        let error_message_input = e.target.parentNode.lastChild;
+        var string = e.target.value;
+        
+        var error_message = "";
+        if (passed_case === 'username') {
+            if (string === "") {
+                error_message = "Fill out field";
+            } else if (/^[a-zA-Z0-9]{5,}$/.test(string) === false) {
+                error_message = "Must be 5 characters long and can contain both letters and numbers";
+            }
+        }
+        if (passed_case === 'select') {
+            if (string === "") {
+                error_message = "Fill out field";
+            } else if (string <= 0 | string === null) {
+                error_message = "Must select a valid option";
+            }
+        }
+        if (passed_case === 'phone') {
+            if (string === "") {
+                error_message = "Fill out field";
+            } else if (/^[+ ]?[( ]?[0-9 ]{3}[) ]?[-\s. ]?[0-9 ]{3}[-\s. ]?[0-9 ]{4,6}$/.test(string) === false) {
+                error_message = "Must be a valid phone number";
+            }
+            string = string.replace("/", "");
+            string = string.replace("+", "");
+            string = string.replace("_", "");
+            string = string.replace("-", "");
+            string = string.replace("(", "");
+            string = string.replace(")", "");
+            string = string.replace(".", "");
+            e.target.value = string;
+        }
+        if (passed_case === 'email') {
+            if (string === "") {
+                error_message = "Fill out field";
+            } else if (/^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/.test(string) === false) {
+                error_message = "Must be a valid email address";
+            }
+        }
+        if (passed_case === 'name') {
+            if (string === "") {
+                error_message = "Fill out field";
+            } else if (/^([a-zA-Z' ]+)$/.test(string) === false) {
+                error_message = "Must contain only letters";
+            }
+        }
+        // //all the checks for correct fields
+      
+        error_message_input.innerHTML = error_message;
+        var form_loop_id = e.target.parentNode.parentNode.parentNode;
+        var form_submit_button = e.target.parentNode.parentNode.parentNode.lastChild;
+        
+        var error_messages = form_loop_id.querySelectorAll('.error_message');
+        let status_of_form = true;
+        for (var i = 0; i < error_messages.length; ++i) {
+            if (error_messages[i].textContent.length > 0) {
+                status_of_form = false;
+            }
+        }
+        if (status_of_form === true) {
+            form_submit_button.disabled = false;
+        } else if (status_of_form === false) {
+            form_submit_button.disabled = true;
+        }
+    }
     handleSubmit = (e) => {
         e.preventDefault();
         var that = this;
         const newUser = {users_id: "Undefined", username:e.target.create_username.value, password:"No Password", first_name: e.target.create_first.value, 
         last_name: e.target.create_last.value, email: e.target.create_email.value, 
-        phone: e.target.create_phone.value, job_type_id: e.target.job_type_user.value, access_rights: e.target.access_rights_user.value}
-        that.props.UsersActionsAdd(newUser);
-        
+        phone: e.target.create_phone.value, job_type_id: e.target.job_type_user.value, access_rights: e.target.access_rights_user.value}        
+        var url = "https://localhost:443/api/ws.php";
+        var formData = new FormData();
+        formData.append('admin', 'create_new_user');
+        formData.append('admin_create_new_details', JSON.stringify(newUser));
+        formData.append('admin_user', this.props.status[0].login_state);
+        fetch(url, {
+            method: "POST",
+            body: formData,
+            // credentials: 'include'
+        })
+            .then(function (response) {
+                if (response.status !== 200) {
+                    console.log(
+                        "Looks like there was a problem. Status Code: " + response.status
+                    );
+                    return;
+                }
+                response.json().then(function (data) {
+                    if (!data.error) {
+                        newUser.users_id = data
+                        that.props.UsersActionsAdd(newUser);
+                        
+                        M.toast({ html: "User Created", displayLength: 10000 });
+                    } else {
+                        M.toast({ html: "Error", displayLength: 10000 });
+                        // document.getElementById("toast-container").addEventListener("click", toasthide)
+                    }
+                    document.getElementById('form_create_employee').reset();
+                });
+            })
+            .catch(function (err) {
+                console.log("Fetch Error :-S", err);
+            });
     }
     render() {
         const { formtype } = this.props;
@@ -39,7 +138,7 @@ class CreateNewForm extends Component {
                                 <div className="row input-centre">
                                     <div className="input-field main_color_four col s12">
                                         <input type="text" className="input-boxes2" name="create_username" id="create_username"
-                                            required />
+                                            onChange={this.validation("username")} required />
                                         <label htmlFor="create_username">Username</label>
                                         <div className="error_message"></div>
                                     </div>
@@ -47,7 +146,7 @@ class CreateNewForm extends Component {
                                 <div className="row input-centre">
                                     <div className="input-field main_color_four col s12">
                                         <input type="text" className="input-boxes2" name="first" id="create_first"
-                                            required />
+                                            onChange={this.validation("name")} required />
                                         <label htmlFor="create_first">First Name</label>
                                         <div className="error_message"></div>
                                     </div>
@@ -55,7 +154,7 @@ class CreateNewForm extends Component {
                                 <div className="row input-centre">
                                     <div className="input-field main_color_four col s12">
                                         <input type="text" className="input-boxes2" name="last" id="create_last"
-                                            required />
+                                            onChange={this.validation("name")} required />
                                         <label htmlFor="create_last">Last Name</label>
                                         <div className="error_message"></div>
                                     </div>
@@ -63,14 +162,14 @@ class CreateNewForm extends Component {
                                 <div className="row input-centre">
                                     <div className="input-field main_color_four col s12">
                                         <input type="text" className="input-boxes2" name="email" id="create_email"
-                                            required />
+                                            onChange={this.validation("email")} required />
                                         <label htmlFor="create_email">Email</label>
                                         <div className="error_message"></div>
                                     </div>
                                 </div>
                                 <div className="row input-centre">
                                     <div className="input-field main_color_four col s12">
-                                        <input type="text" className="input-boxes2" name="phone" id="create_phone" required />
+                                        <input type="text" className="input-boxes2" name="phone" id="create_phone" onChange={this.validation("phone")} required />
                                         <label htmlFor="create_phone">Phone</label>
                                         <div className="error_message"></div>
                                     </div>
@@ -78,7 +177,7 @@ class CreateNewForm extends Component {
                                 <div className="row input-centre">
                                     <div className="input-field main_color_four col s12">
                                         <select name="job_type_user" className="browser-default" id="job_type_user"
-                                            required>
+                                            onChange={this.validation("select")} required>
                                             {JobTypeList}
                                         </select>
                                         <div className="error_message"></div>
@@ -87,7 +186,7 @@ class CreateNewForm extends Component {
                                 <div className="row input-centre">
                                     <div className="input-field main_color_four col s12">
                                         <select name="access_rights_user" className="browser-default" id="access_rights_user"
-                                            required>
+                                            onChange={this.validation("select")} required>
                                                 <option value="1">Employee: 1</option>
                                                 <option value="2">Manager: 2</option>
                                                 <option value="3">Admin: 3</option>
@@ -110,7 +209,8 @@ class CreateNewForm extends Component {
 }
 const mapStateToProps = (state) => {
     return {
-        job_type: state.job_type.job_types
+        job_type: state.job_type.job_types,
+        status: state.auth.login_state
     }
 }
 const mapDispatchToProps = (dispatch) => {
--- src/components/Data/UpdateForm.js
+++ src/components/Data/UpdateForm.js
@@ -1,8 +1,77 @@
 import React, { Component } from "react";
 import { connect } from 'react-redux'
 import { UsersActionsUpdate } from "../../actions/UsersActions";
+import M from 'materialize-css'
 
 class UpdateForm extends Component {
+    validation = passed_case => e => {
+        let error_message_input = e.target.parentNode.lastChild;
+        var string = e.target.value;
+        
+        var error_message = "";
+        if (passed_case === 'username') {
+            if (string === "") {
+                error_message = "Fill out field";
+            } else if (/^[a-zA-Z0-9]{5,}$/.test(string) === false) {
+                error_message = "Must be 5 characters long and can contain both letters and numbers";
+            }
+        }
+        if (passed_case === 'select') {
+            if (string === "") {
+                error_message = "Fill out field";
+            } else if (string <= 0 | string === null) {
+                error_message = "Must select a valid option";
+            }
+        }
+        if (passed_case === 'phone') {
+            if (string === "") {
+                error_message = "Fill out field";
+            } else if (/^[+ ]?[( ]?[0-9 ]{3}[) ]?[-\s. ]?[0-9 ]{3}[-\s. ]?[0-9 ]{4,6}$/.test(string) === false) {
+                error_message = "Must be a valid phone number";
+            }
+            string = string.replace("/", "");
+            string = string.replace("+", "");
+            string = string.replace("_", "");
+            string = string.replace("-", "");
+            string = string.replace("(", "");
+            string = string.replace(")", "");
+            string = string.replace(".", "");
+            e.target.value = string;
+        }
+        if (passed_case === 'email') {
+            if (string === "") {
+                error_message = "Fill out field";
+            } else if (/^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/.test(string) === false) {
+                error_message = "Must be a valid email address";
+            }
+        }
+        if (passed_case === 'name') {
+            if (string === "") {
+                error_message = "Fill out field";
+            } else if (/^([a-zA-Z' ]+)$/.test(string) === false) {
+                error_message = "Must contain only letters";
+            }
+        }
+        // //all the checks for correct fields
+      
+        error_message_input.innerHTML = error_message;
+        var form_loop_id = e.target.parentNode.parentNode.parentNode;
+        var form_submit_button = e.target.parentNode.parentNode.parentNode.lastChild;
+        
+        var error_messages = form_loop_id.querySelectorAll('.error_message');
+        let status_of_form = true;
+        for (var i = 0; i < error_messages.length; ++i) {
+            if (error_messages[i].textContent.length > 0) {
+                status_of_form = false;
+            }
+        }
+        if (status_of_form === true) {
+            form_submit_button.disabled = false;
+        } else if (status_of_form === false) {
+            form_submit_button.disabled = true;
+        }
+    }
+
     handlePassClick = (e) => {
         e.preventDefault();
         if (e.currentTarget.parentNode.firstChild.firstChild.value === "***") {
@@ -13,7 +82,6 @@ class UpdateForm extends Component {
     }
     handleSubmit = (e) => {
         e.preventDefault();
-        var that = this;
         const updateUser = {
             users_id: e.target.update_id.value, 
             username: e.target.update_username.value, 
@@ -25,7 +93,37 @@ class UpdateForm extends Component {
             job_type_id: e.target.update_job_type_user.value, 
             access_rights: e.target.access_rights_user.value
         }
-        that.props.UsersActionsUpdate(updateUser);
+        var that = this;
+        var url = "https://localhost:443/api/ws.php";
+        var formData = new FormData();
+        formData.append('admin', 'update_user');
+        formData.append('admin_update_user', JSON.stringify(updateUser));
+        formData.append('admin_user', this.props.status[0].login_state);
+        fetch(url, {
+            method: "POST",
+            body: formData,
+            // credentials: 'include'
+        })
+            .then(function (response) {
+                if (response.status !== 200) {
+                    console.log(
+                        "Looks like there was a problem. Status Code: " + response.status
+                    );
+                    return;
+                }
+                response.json().then(function (data) {
+                    if (!data.error) {
+                        that.props.UsersActionsUpdate(updateUser);
+                        M.toast({ html: "User Updated ", displayLength: 10000 });
+                    } else {
+                        M.toast({ html: "Error", displayLength: 10000 });
+                        // document.getElementById("toast-container").addEventListener("click", toasthide)
+                    }
+                });
+            })
+            .catch(function (err) {
+                console.log("Fetch Error :-S", err);
+            });
 
     }
 
@@ -50,18 +148,18 @@ class UpdateForm extends Component {
                     action="model/ws.php" method="POST">
                     <h5 className="main_text">Update {updatetype}</h5>
                     <input type="hidden" name="error" />
-                    <input name="update_id" id="update_id" hidden  defaultValue={user.users_id}/>
+                    <input name="update_id" id="update_id" hidden  defaultValue={user.users_id} />
                     <div className="row input-centre">
                         <div className="input-field main_color_four col s12">
                             <input type="text" className="input-boxes2" name="update_username" id="update_username"
-                                defaultValue={user.username} required />
+                                defaultValue={user.username} onChange={this.validation("username")} required />
                             <label className="active" htmlFor="update_username">Username</label>
                             <div className="error_message"></div>
                         </div>
                     </div>
                     <div className="row">
                         <div className="input-field main_color_four col s10">
-                            <input type="text" className="input-boxes2" name="password" id="update_password" readOnly value={user.password} required />
+                            <input type="text" className="input-boxes2" name="password" id="update_password" onChange={this.validation("update_pass")} readOnly value={user.password} required />
                             <label className="active" htmlFor="update_password">Password</label>
                             <div className="error_message"></div>
                         </div>
@@ -70,7 +168,7 @@ class UpdateForm extends Component {
                     <div className="row input-centre">
                         <div className="input-field main_color_four col s12">
                             <input type="text" className="input-boxes2" name="first" id="update_first"
-                                defaultValue={user.first_name} required />
+                                defaultValue={user.first_name} onChange={this.validation("name")} required />
                             <label className="active" htmlFor="update_first">First Name</label>
                             <div className="error_message"></div>
                         </div>
@@ -78,7 +176,7 @@ class UpdateForm extends Component {
                     <div className="row input-centre">
                         <div className="input-field main_color_four col s12">
                             <input type="text" className="input-boxes2" name="last" id="update_last"
-                                defaultValue={user.last_name} required />
+                                defaultValue={user.last_name} onChange={this.validation("name")} required />
                             <label className="active" htmlFor="update_last">Last Name</label>
                             <div className="error_message"></div>
                         </div>
@@ -86,14 +184,14 @@ class UpdateForm extends Component {
                     <div className="row input-centre">
                         <div className="input-field main_color_four col s12">
                             <input type="text" className="input-boxes2" name="email" id="update_email"
-                                defaultValue={user.email} required />
+                                defaultValue={user.email} onChange={this.validation("email")} required />
                             <label className="active" htmlFor="update_email">Email</label>
                             <div className="error_message"></div>
                         </div>
                     </div>
                     <div className="row input-centre">
                         <div className="input-field main_color_four col s12">
-                            <input type="text" className="input-boxes2" name="phone" id="update_phone" defaultValue={user.phone} required />
+                            <input type="text" className="input-boxes2" name="phone" id="update_phone" defaultValue={user.phone} onChange={this.validation("phone")} required />
                             <label className="active" htmlFor="update_phone">Phone</label>
                             <div className="error_message"></div>
                         </div>
@@ -101,7 +199,7 @@ class UpdateForm extends Component {
                     <div className="row input-centre">
                         <div className="input-field main_color_four col s12">
                             <select name="update_job_type_user" className="browser-default" id="update_job_type_user"
-                                required>
+                                onChange={this.validation("select")} required>
                                 {JobTypeList}
                             </select>
                             <div className="error_message"></div>
@@ -110,7 +208,7 @@ class UpdateForm extends Component {
                     <div className="row input-centre">
                         <div className="input-field main_color_four col s12">
                             <select name="access_rights_user" className="browser-default" id="access_rights_user"
-                                required>
+                                onChange={this.validation("select")} required>
                                 <option value="1">Employee: 1</option>
                                 <option value="2">Manager: 2</option>
                                 <option value="3">Admin: 3</option>
@@ -130,7 +228,8 @@ class UpdateForm extends Component {
 }
 const mapStateToProps = (state) => {
     return {
-        job_type: state.job_type.job_types
+        job_type: state.job_type.job_types,
+        status: state.auth.login_state
     }
 }
 const mapDispatchToProps = (dispatch) => {
